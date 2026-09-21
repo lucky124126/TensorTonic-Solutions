49d46a6febdceb170f1d77c6ac9cb0288ce8fd6c
lucky124126 <lucky.tekwani24@vit.edu>
2026-09-21 13:43:06 +0530

    Solve tanh-activation

---
+++ tanh-activation/tanh-activation.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+def tanh(x: list) -> np.ndarray:
+    """
+    Returns a NumPy array with the same shape as x.
+    """
+    # Write code here
+    x =np.array(x)
+    
+    
+    return  (np.exp(x) - np.exp(-x))/(np.exp(x) + np.exp(-x))
+    
